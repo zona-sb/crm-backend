512716ed0b18efb964234722bbe6309bd20d6a7b
Даниил Андреев <104764384+AndreevDan93@users.noreply.github.com>
2023-07-04 16:58:44 +0500

    Init (#3)
    * add git ignore
    
    * init project
    
    * init project
    
    * init project

--- src/main/java/ru/zonasb/backend/ZonasbApplication.java
+++ src/main/java/ru/zonasb/backend/ZonasbApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZonasbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZonasbApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZonasbApplication.class, args);
+    }
 
 }
+
---
+++ src/main/java/ru/zonasb/backend/model/User.java
@@ -0,0 +1,5 @@
+package ru.zonasb.backend.model;
+
+
+public class User {
+}
