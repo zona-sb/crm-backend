50e7b91333b781e8e7f369b4f13a039e7b3d205a
Даниил Андреев <104764384+AndreevDan93@users.noreply.github.com>
2023-07-05 16:17:59 +0500

    Security (#5)
    * init security
    
    * add security

---
+++ src/main/java/ru/zonasb/backend/service/user/UserService.java
@@ -0,0 +1,19 @@
+package ru.zonasb.backend.service.user;
+
+import ru.zonasb.backend.dto.UserDto;
+import ru.zonasb.backend.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User getUserById(long id);
+    List<User> getAllUsers();
+    User createNewUser(UserDto userDto);
+
+    User updateUser(long id, UserDto userDto);
+
+    String getCurrentUserName();
+
+    User getCurrentUser();
+}
