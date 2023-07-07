ed8454aa1317a04efc123d22029deb4f65d183dc
AndreevDan93 <andreevdan93@gmail.com>
2023-07-07 19:21:53 +0500

    fixes

--- src/main/java/ru/zonasb/backend/ZonasbApplication.java
+++ src/main/java/ru/zonasb/backend/ZonasbApplication.java
@@ -6,18 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZonasbApplication {
 
-<<<<<<< HEAD
-
     public static void main(String[] args) {
         SpringApplication.run(ZonasbApplication.class, args);
     }
-
 }
-
-=======
-	public static void main(String[] args) {
-		SpringApplication.run(ZonasbApplication.class, args);
-	}
-
-}
->>>>>>> 98de029 (Init (#1))
