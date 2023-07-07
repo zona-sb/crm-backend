092b28652115fb55bed26ea0f410f26bebb2f1d8
AndreevDan93 <andreevdan93@gmail.com>
2023-07-07 19:47:50 +0500

    fixes

--- src/main/java/ru/zonasb/backend/dto/people/WorkerDto.java
+++ src/main/java/ru/zonasb/backend/dto/people/WorkerDto.java
@@ -1,7 +1,6 @@
 package ru.zonasb.backend.dto.people;
 
 import jakarta.persistence.Column;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,16 +15,13 @@ public class WorkerDto {
     private Long id;
 
     @NotBlank(message = "name should not be Empty")
-    @Column(name = "name")
     private String name;
 
     @NotBlank(message = "phone should not be Empty")
-    @Column(name = "phone", unique = true)
     @Size(min = 11, max = 11, message = "phone should be 11 numbers")
     private String phone;
 
     @NotBlank(message = "email should not be Empty")
-    @Email(message = "Incorrect Email")
     @Column(name = "email", unique = true)
     private String email;
 }
