c2c67e4b1db93807f7a87ba779d0de88ab78f448
AndreevDan93 <andreevdan93@gmail.com>
2023-07-10 15:15:50 +0500

    add crud logic for client and worker

--- src/main/java/ru/zonasb/backend/service/client/ClientService.java
+++ src/main/java/ru/zonasb/backend/service/client/ClientService.java
@@ -3,8 +3,12 @@ package ru.zonasb.backend.service.client;
 import ru.zonasb.backend.dto.people.ClientDto;
 import ru.zonasb.backend.model.people.Client;
 
+import java.util.List;
+
 public interface ClientService {
     Client getClientById(long id);
-    ClientDto createNewClient(ClientDto clientDto);
-    ClientDto updateClientById(long id, ClientDto clientDto);
+    List<Client> getAllClients();
+    Client createNewClient(ClientDto clientDto);
+    Client updateClientById(long id, ClientDto clientDto);
+    void deleteById(long id);
 }
