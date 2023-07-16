0cf3853d855fba8bbd91bb6e127ccffa1c6144a6
AndreevDan93 <andreevdan93@gmail.com>
2023-07-16 23:53:22 +0500

    fix phone validation

--- src/main/java/ru/zonasb/backend/api/viber/ViberWebhookController.java
+++ src/main/java/ru/zonasb/backend/api/viber/ViberWebhookController.java
@@ -1,16 +1,30 @@
 package ru.zonasb.backend.api.viber;
 
-import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class ViberWebhookController {
 
     @PostMapping("/webhook")
-    public ResponseEntity<String> handleWebhook(@RequestBody String payload) {
+    public String handleWebhook(@RequestBody String payload) {
+        WebhookResponse webhookResponse = WebhookResponse.builder()
+                .status(0)
+                .status_message("ok")
+                .event_types(new ArrayList<>())
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
         // Здесь вы можете обработать входящий запрос от Viber API
-        return ResponseEntity.ok("Webhook received");
+        try {
+            return objectMapper.writeValueAsString(webhookResponse);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
