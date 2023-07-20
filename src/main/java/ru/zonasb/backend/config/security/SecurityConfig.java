2a5de644049d3395e92b395e00c0df8c4348ecc8
AndreevDan93 <andreevdan93@gmail.com>
2023-07-20 13:51:08 +0500

    fix phone validation

--- src/main/java/ru/zonasb/backend/config/security/SecurityConfig.java
+++ src/main/java/ru/zonasb/backend/config/security/SecurityConfig.java
@@ -20,9 +20,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.zonasb.backend.config.component.JWTHelper;
 import ru.zonasb.backend.config.filter.JWTAuthenticationFilter;
 import ru.zonasb.backend.config.filter.JWTAuthorizationFilter;
@@ -88,7 +85,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+//                .cors().and()
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers(publicUrls).permitAll()
@@ -108,10 +105,10 @@ public class SecurityConfig {
 
         return http.build();
     }
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+//        return source;
+//    }
 }
