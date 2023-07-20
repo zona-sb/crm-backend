75af0459afe9dad55cf5052a666cedd040b04b5a
AndreevDan93 <andreevdan93@gmail.com>
2023-07-20 15:39:15 +0500

    fix phone validation

--- src/main/java/ru/zonasb/backend/config/security/SecurityConfig.java
+++ src/main/java/ru/zonasb/backend/config/security/SecurityConfig.java
@@ -109,19 +109,11 @@ public class SecurityConfig {
 
         return http.build();
     }
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-//        return source;
-//    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-//        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowedHeaders(Arrays.asList("*"));
-//        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
