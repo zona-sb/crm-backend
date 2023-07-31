478aaed91ff05f908c230d4c648afab8d5bd1444
Даниил Андреев <104764384+AndreevDan93@users.noreply.github.com>
2023-07-31 23:54:28 +0500

    Dev (#52)
    * fix folders (#41)
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation
    
    * fix phone validation (#44)
    
    * Fix bugs (#47)
    
    * Bugs fixed in status, category, priority
    
    * bug fixes
    
    * fix (#51)
    
    ---------
    
    Co-authored-by: Ксения Доможилова <111085352+xushaha@users.noreply.github.com>

--- src/main/java/ru/zonasb/backend/repository/ClientRepository.java
+++ src/main/java/ru/zonasb/backend/repository/ClientRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.zonasb.backend.model.people.Client;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findClientByPersonEmail(String email);
+    Optional<Client> findClientByPersonPhone(String phone);
 }
