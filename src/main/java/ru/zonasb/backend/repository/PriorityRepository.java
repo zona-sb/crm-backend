e5beaa611ca9ec3f15d9a5ccbdcaba43defbbb6e
Ксения Доможилова <111085352+xushaha@users.noreply.github.com>
2023-09-03 17:26:44 +0300

    Fixed priority filtration (#68)

--- src/main/java/ru/zonasb/backend/repository/PriorityRepository.java
+++ src/main/java/ru/zonasb/backend/repository/PriorityRepository.java
@@ -1,14 +1,31 @@
 package ru.zonasb.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 import ru.zonasb.backend.model.tasks.Priority;
+import ru.zonasb.backend.model.tasks.QPriority;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 
 import java.util.Optional;
 
 @Repository
-public interface PriorityRepository extends JpaRepository<Priority, Long>{
+public interface PriorityRepository extends JpaRepository<Priority, Long>, QuerydslPredicateExecutor<Priority>, QuerydslBinderCustomizer<QPriority> {
     Optional<Priority> findPriorityByTitle(String title);
     Optional<Priority> findPriorityByWeight(int priorityWeight);
     Optional<Priority> findPriorityByColor(String color);
+
+    @Override
+    default void customize(QuerydslBindings bindings, QPriority priority) {
+
+        bindings.bind(priority.title, priority.color)
+                .first(StringExpression::containsIgnoreCase);
+
+        bindings.bind(priority.weight).first(SimpleExpression::eq);
+
+    }
+
 }
